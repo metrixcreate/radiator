5357967291231880a12feaef7a24ad688a8d6d29
Metrix <metrix@Blended.(none)>
2010-03-31 12:25:47 -0700

    adding patched arduino-17

--- scripts/skeinall.sh
+++ scripts/skeinall.sh
@@ -1,3 +1,3 @@
-for P in `find mendel -name '*.stl' -print` ; do nice python skeinforge/skeinforge_tools/craft_plugins/export.py $P ; done && cd mendel ; find . -name '*.gcode' -exec 'cp' '{}' '../mendel-milo/{}' ';' -print && cd .. && tar czvf /home/metrix/Desktop/mendel-milo.tar mendel-milo
+for P in `find mendel_sleds -name '*.stl' -print` ; do python /home/metrix/radiator/skeinforge_mcs_mendel/skeinforge_tools/craft_plugins/export.py $P ; done && cd mendel_sleds; find . -name '*.gcode' -exec 'cp' '{}' '../mendel_sleds/{}' ';' -print && cd .. && tar czvf /home/metrix/radiator/sleds.tar mendel_sleds
 
 
